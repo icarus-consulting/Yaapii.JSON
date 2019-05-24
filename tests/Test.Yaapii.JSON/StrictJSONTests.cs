b6d4059ce26f79af8dfeee0721ae8bb87cbe7612
lwuLok <lukas.wulff@icarus-consult.de>
2019-05-24 15:06:28 +0200

    test: updated test for additional contructor

--- tests/Test.Yaapii.JSON/StrictJSONTests.cs
+++ tests/Test.Yaapii.JSON/StrictJSONTests.cs
@@ -1,5 +1,5 @@
 ﻿using Xunit;
-using Yaapii.JSON;
+using Yaapii.Atoms.IO;
 
 namespace Yaapii.JSON.Test
 {
@@ -27,6 +27,33 @@ namespace Yaapii.JSON.Test
                 "a word",
                 new StrictJSON(
                     new JSONOf(json),
+                    schema
+                ).Value("test")
+            );
+        }
+
+        [Fact]
+        public void RejectsInvalidInput()
+        {
+            var json = "{ \"test\": \"a word\" }";
+            var schema = "{ \"type\": \"object\", \"properties\": { \"test\": { \"type\": \"number\" } } }";
+
+            Assert.Throws<InvalidJSONException>(() =>
+            {
+                new StrictJSON(new InputOf(json), schema).Value("test");
+            });
+        }
+
+        [Fact]
+        public void AcceptsValidInput()
+        {
+            var json = "{ \"test\": \"a word\" }";
+            var schema = "{ \"type\": \"object\", \"properties\": { \"test\": { \"type\": \"string\" } } }";
+
+            Assert.Equal(
+                "a word",
+                new StrictJSON(
+                    new InputOf(json),
                     schema
                 ).Value("test")
             );
